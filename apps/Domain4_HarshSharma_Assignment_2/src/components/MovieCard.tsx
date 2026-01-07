aa6f7214b3eae17eff879573634acf4dcb286029
harsh091234 <hsharma4090@gmail.com>
2026-01-07 21:45:40 +0530

    .

--- apps/Domain4_HarshSharma_Assignment_2/src/components/MovieCard.tsx
+++ apps/Domain4_HarshSharma_Assignment_2/src/components/MovieCard.tsx
@@ -1,7 +1,7 @@
-import React from 'react'
+
 import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
 import { Badge } from "@/components/ui/badge";
-import { AspectRatio } from "@/components/ui/aspect-ratio";
+
 import { Star } from "lucide-react";
 import { useNavigate } from 'react-router-dom';
 
